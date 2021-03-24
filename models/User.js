5c948b7b9851b2b30335d745793edfd58f4446bb
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-03-23 19:54:35 -0700

    testing a PR to see if other members get latest stuff from main

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+const bcrypt = require('bcrypt');
+
+
+class User extends Model {
+    //   checkPassword(loginPW) {
+    //     console.log(loginPW)
+    //    return bcrypt.compareSync(loginPW, this.password);
+    //   }
+};
+
+User.init(
+    {
+        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+  //      hooks: {
+            //    set up beforeCreate lifecycle "hook" functionality
+      //      async beforeCreate(newUserData) {
+       //         newUserData.password = await bcrypt.hash(newUserData.password, 10);
+         //       return newUserData;
+        //    },
+
+        //    async beforeUpdate(updatedUserData) {
+         //       updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          //      return updatedUserData;
+         //   }
+      //  },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+
+module.exports = User;
