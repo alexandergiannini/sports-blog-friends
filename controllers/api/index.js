49970e036bc06b1c425d23d59a5b895ab5a9f626
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-03-24 20:53:24 -0700

    added routes for post-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,12 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 
+const postRoutes = require('./post-routes.js');
+
+
 router.user('/users', userRoutes);
+
+router.use('/posts', postRoutes);
+
 
 module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,1 +1,64 @@
 //test
+
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        //   include: [User]
+    }).then(result => {
+        res.json(result);
+    })
+});
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        //   include: [User]
+    }).then(result => {
+        res.json(result);
+    })
+});
+
+
+router.post('/', (req, res) => {
+    Post.create({
+        // id: req.body.id,
+        post_title: req.body.post_title,
+        post_content: req.body.post_content,  //req.body.body,
+        // user_id: req.session.user_id, ///req.body.user_id
+        //   user: req.session.username
+    }).then(result => {
+        res.json(result);
+    })
+
+});
+///updating a post
+router.put('/:id', (req, res) => {
+    // update a post by its `id` value
+    Post.update(
+        req.body
+        , {
+            where: {
+                id: req.params.id
+            }
+        },
+    ).then(result => {
+        res.json(result);
+    })
+});
+
+/////deleting a post endpoint
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(result => {
+        res.json(result);
+    })
+});
+
+module.exports = router;
