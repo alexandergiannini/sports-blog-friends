67472d14159f5ca57e2548497d82c0d0b81b2b70
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-03-24 21:08:06 -0700

    add the path for comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,10 +5,11 @@ const userRoutes = require('./user-routes.js');
 
 const postRoutes = require('./post-routes.js');
 
+const commentRoutes = require('./comment-routes');
 
 router.user('/users', userRoutes);
 
 router.use('/posts', postRoutes);
-
+router.use('/comments', commentRoutes);
 
 module.exports = router;
