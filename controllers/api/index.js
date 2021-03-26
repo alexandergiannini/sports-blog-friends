a7a31580764c7ca80931e1eef10092bea44be6c1
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-03-25 20:38:38 -0700

    Added a home route when we run the server

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,9 +7,13 @@ const postRoutes = require('./post-routes.js');
 
 const commentRoutes = require('./comment-routes');
 
+
+
 router.use('/users', userRoutes);
 
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+
+
 
 module.exports = router;
