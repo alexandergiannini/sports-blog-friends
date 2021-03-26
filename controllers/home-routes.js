dfa253ecdfc07c85fc5dda39eb0b0a007753eb1e
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-03-25 22:12:44 -0700

    added the homepage route correctly

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,8 +3,35 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 
+
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        attributes: [
+          'id',
+          'post_title',
+          'post_content',
+          'user_id'
+        ]
+        ,
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'user_id', 'post_id', 'comment_text'] //'createdAt', 'updatedAt'
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+      ]
+      }).then(dbPostData => {
+        console.log(dbPostData);
+        console.log(req.session);
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+      }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 
 
     });
